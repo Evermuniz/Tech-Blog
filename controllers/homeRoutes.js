9c6a23ddc8cc6daa7eaa0bc2c063d54be1c8d517
Evermuniz <evermuniz@outlook.com>
2023-06-16 17:43:23 -0600

    finally got the comments working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,10 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
-const withAuth = require('../utils/auth'); 
+const { Post, User, Comments } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-        const postData = await Post.findAll({
-            include: [
-                {
-                    model: User,
-                    attributes: ['name'],
-                },
-            ],
-        });
-        const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('homepage', {
-            posts,
-            logged_in: req.session.logged_in,
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-}
-);
-
-router.get('/post/:id', async (req, res) => {
   try {
-    const postData = await Post.findByPk(req.params.id, {
+    const postData = await Post.findAll({
       include: [
         {
           model: User,
@@ -33,11 +12,29 @@ router.get('/post/:id', async (req, res) => {
         },
       ],
     });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: Comments, include: { model: User, attributes: ['name'] }}],
+    });
+    console.log(postData);
 
     const post = postData.get({ plain: true });
+    console.log(post);
 
     res.render('post', {
-      post: post,
+      ...post,
+      ...Comments,
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -46,12 +43,11 @@ router.get('/post/:id', async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
-}
-);
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  res.render('login');
+});
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,23 +1,32 @@
 const User = require('./User');
 const Post = require('./Post');
-const Comment = require('./Comment');
+const Comments = require('./Comments');
 
 User.hasMany(Post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'created_by',
+  onDelete: 'CASCADE',
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: 'created_by',
 });
 
-Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: 'CASCADE'
+Post.hasMany(Comments, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE',
 });
 
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
+Comments.belongsTo(Post, {
+  foreignKey: 'post_id',
 });
 
-module.exports = { User, Post, Comment };
+User.hasMany(Comments, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Post, Comments };
