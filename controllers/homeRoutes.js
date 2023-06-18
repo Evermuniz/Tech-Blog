162f1c1eaf1a219f056efa35b5a3c5a09350d132
Evermuniz <evermuniz@outlook.com>
2023-06-18 12:23:25 -0600

    new post and delete

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,7 +22,50 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/post/:id', async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/myposts/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const post = postData.get({ plain: true });
+
+    res.render('myposts', {
+      ...post,
+      logged_in: req.session.logged_in,
+      post: [post],
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+router.get('/post/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [{ model: Comments, include: { model: User, attributes: ['name'] }}],
@@ -41,6 +84,8 @@ router.get('/post/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
